a80f1d5860547a8d7a5665991e5c0a1a1febeaf8
lamtrinhh1201 <lamtrinhh1201@gmail.com>
2025-04-16 16:36:44 +0700

    Add GateTargetManager

---
+++ BridgeRace_trinhltl/Assets/_GAME/Scripts/Bridge/GateTargetManager.cs
@@ -0,0 +1,73 @@
+namespace _GAME.Scripts.FSM.Bridge
+{
+    using System;
+    using System.Collections.Generic;
+    using _GAME.Scripts.Character;
+    using _GAME.Scripts.Floor;
+    using UnityEngine;
+
+    public class GateTargetManager : MonoBehaviour
+    {
+        public static GateTargetManager Instance { get; private set; }
+
+        private Dictionary<int, BotController> _reservedGates = new Dictionary<int, BotController>();
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+
+        public bool IsGateAvailable(int gateIndex)
+        {
+            return !this._reservedGates.ContainsKey(gateIndex);
+        }
+
+        public bool ReserveGate(int gateIndex, BotController bot)
+        {
+            if (IsGateAvailable(gateIndex))
+            {
+                this._reservedGates[gateIndex] = bot;
+                return true;
+            }
+            return false;
+        }
+
+        public void ReleaseGates(int gateIndex, BotController bot)
+        {
+            if (this._reservedGates.TryGetValue(gateIndex, out var reservedBot) && reservedBot == bot)
+            {
+                this._reservedGates.Remove(gateIndex);
+            }
+        }
+
+        public int NearestAvailableGate(BotController bot, List<FloorGate> gates)
+        {
+            var bestGateIndex   = -1;
+            var closestDistance = float.MaxValue;
+
+            for (var i = 0; i < gates.Count; i++)
+            {
+                if (!IsGateAvailable(i))
+                {
+                    continue;
+                }
+
+                var distance = Vector3.Distance(bot.transform.position, gates[i].transform.position);
+
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    bestGateIndex   = i;
+                }
+            }
+            return bestGateIndex;
+        }
+    }
+}
